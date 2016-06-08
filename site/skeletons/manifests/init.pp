b6338692c9140bf4447d61af6d1cfe1681567ef7
tyronzerafa <tyron.zerafa@gmail.com>
2016-06-08 13:10:19 +0200

    exercise 9.3

--- site/skeletons/manifests/init.pp
+++ site/skeletons/manifests/init.pp
@@ -3,13 +3,13 @@ class skeleton {
 file { '/etc/skel':
   ensure => directory,
   user => 'root',
-  group = 'root'
+  group => 'root'
 }
 
   file { '/etc/skel/.bashrc':
     ensure => file,
     user => 'root',
-    group = 'root',
+    group => 'root',
     source => 'puppet:///modules/skeleton/bashrc',
   }
 }
